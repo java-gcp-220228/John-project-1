acb5f7da5914b482143257cf9a5ce71d20b2d2a3
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2022-03-26 18:55:52 -0400

    Filter function now uses obversable impl

--- ui/src/app/dashboard/dashboard.component.ts
+++ ui/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { ApiService } from '../api.service';
 import { TicketDialog } from '../dialog/ticket.dialog';
 import { Ticket } from '../model/ticket.model';
@@ -14,7 +14,6 @@ import { User } from '../model/user.model';
 export class DashboardComponent {
   cards!: Observable<Ticket[]>;
   role = localStorage.getItem('user_role');
-  filter: string = 'ALL';
   statuses: string[] = ['ALL', 'PENDING', 'APPROVED', 'DENIED'];
 
   constructor(
@@ -22,11 +21,16 @@ export class DashboardComponent {
     private dialog: MatDialog,
     ) {
       if (this.role?.toUpperCase().includes('MANAGER')) {
-        this.cards = this.api.getTickets();
+        this.cards = this.filterTickets('ALL');
       } else if (this.role?.toUpperCase().includes('EMPLOYEE')) {
         let userId = localStorage.getItem('user_id');
         this.cards = this.api.getEmployeeTickets(Number(userId));
       }
+    }
+
+    filterTickets(status: string) {
+      if (status == 'ALL') return this.api.getTickets();
+      return this.api.getTickets().pipe(map(tickets => tickets.filter(t => t.status == status)));
     }
 
     newTicket() {
@@ -52,7 +56,7 @@ export class DashboardComponent {
     serveTicket(ticket_id: number, status: string) {
       this.api.patchTicket(ticket_id, status).subscribe({
         next: ticket => {
-          this.cards = this.api.getTickets();
+          this.cards = this.filterTickets(status);
         }
       });
     }
