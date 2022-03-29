57ea9e1a0267e299edbadca34f7b8828fd10047d
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2022-03-28 21:26:28 -0400

    Refactored dao to remove duplicate logic

--- app/src/main/java/com/revature/dao/TicketDao.java
+++ app/src/main/java/com/revature/dao/TicketDao.java
@@ -16,131 +16,84 @@ public class TicketDao {
     private static final String FIRST_NAME = "firstName";
     private static final String LAST_NAME = "lastName";
     private static final String EMAIL = "email";
-    
+
+    private static final String sql = "select tk.reimb_id, tk.reimb_amount, tk.reimb_submitted, " +
+            "tk.reimb_resolved, tk.reimb_description, tk.reimb_receipt, " +
+            "status.reimb_status, rt.reimb_type, " +
+            "json_build_object('id', users.ers_users_id, 'username', users.ers_username, 'firstName'" +
+            ", users.user_first_name, 'lastName', users.user_last_name, 'email', users.user_email) as author, " +
+            "json_build_object('id', users.ers_users_id, 'username', mng.ers_username, 'firstName', mng.user_first_name, 'lastName', " +
+            "mng.user_last_name, 'email', mng.user_email) as resolver " +
+            "from ers_reimbursements as tk " +
+            "inner join ers_reimbursement_status status " +
+            "on tk.reimb_status_id = status.reimb_status_id " +
+            "inner join ers_reimbursement_type rt " +
+            "on tk.reimb_type_id = rt.reimb_type_id " +
+            "inner join ers_users users " +
+            "on tk.reimb_author = users.ers_users_id " +
+            "left join ers_users mng " +
+            "on tk.reimb_resolver = mng.ers_users_id ";
+
     public List<Ticket> getAllTickets() throws SQLException {
         try (Connection con = ConnectionUtility.getConnection()) {
 
-            List<Ticket> tickets = new ArrayList<>();
-
-            String query = "select tk.reimb_id, tk.reimb_amount, tk.reimb_submitted, " +
-                    "tk.reimb_resolved, tk.reimb_description, tk.reimb_receipt, " +
-                    "status.reimb_status, rt.reimb_type, " +
-                    "json_build_object('id', users.ers_users_id, 'username', users.ers_username, 'firstName', users.user_first_name, " +
-                    "'lastName', users.user_last_name, 'email', users.user_email) as author, " +
-                    "json_build_object('id', users.ers_users_id, 'username', mng.ers_username, 'firstName', mng.user_first_name, " +
-                    "'lastName', mng.user_last_name, 'email', mng.user_email) as resolver " +
-                    "from ers_reimbursements as tk " +
-                    "inner join ers_reimbursement_status status " +
-                    "on tk.reimb_status_id = status.reimb_status_id " +
-                    "inner join ers_reimbursement_type rt " +
-                    "on tk.reimb_type_id = rt.reimb_type_id " +
-                    "inner join ers_users users " +
-                    "on tk.reimb_author = users.ers_users_id " +
-                    "left join ers_users mng " +
-                    "on tk.reimb_resolver = mng.ers_users_id";
+            String query = sql;
 
             try (PreparedStatement pstmt = con.prepareStatement(query)) {
 
-                ResultSet rs = pstmt.executeQuery();
-
-                while (rs.next()) {
-                    int id = rs.getInt("reimb_id");
-                    double amount = rs.getDouble("reimb_amount");
-                    Timestamp submitted = rs.getObject("reimb_submitted", Timestamp.class);
-                    Timestamp resolved = rs.getObject("reimb_resolved", Timestamp.class);
-                    String description = rs.getString("reimb_description");
-                    String receiptLink = rs.getString("reimb_receipt");
-                    String status = rs.getString("reimb_status");
-                    String type = rs.getString("reimb_type");
-                    JSONObject jsonObject = new JSONObject(rs.getString("author"));
-                    UserDTO author = new UserDTO(jsonObject.getInt("id"), jsonObject.getString(USERNAME), jsonObject.getString(FIRST_NAME),
-                            jsonObject.getString(LAST_NAME), jsonObject.getString(EMAIL));
-
-                    Ticket ticket = new Ticket(id, amount, submitted, status, type);
-                    ticket.setAuthor(author);
-
-                    if (resolved != null) {
-                        JSONObject json = new JSONObject(rs.getString("resolver"));
-                        UserDTO resolver = new UserDTO(json.getInt("id"), json.getString(USERNAME), json.getString(FIRST_NAME),
-                                json.getString(LAST_NAME), json.getString(EMAIL));
-                        ticket.setResolved(resolved);
-                        ticket.setResolver(resolver);
-                    }
-                    if (description != null) {
-                        ticket.setDescription(description);
-                    }
-                    if (receiptLink != null) {
-                        ticket.setReceiptLink(receiptLink);
-                    }
-                    tickets.add(ticket);
-
-                }
+                return getTickets(pstmt);
             }
-            return tickets;
         }
+    }
+
+    private List<Ticket> getTickets(PreparedStatement pstmt) throws SQLException {
+        List<Ticket> tickets = new ArrayList<>();
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            int id = rs.getInt("reimb_id");
+            double amount = rs.getDouble("reimb_amount");
+            Timestamp submitted = rs.getObject("reimb_submitted", Timestamp.class);
+            Timestamp resolved = rs.getObject("reimb_resolved", Timestamp.class);
+            String description = rs.getString("reimb_description");
+            String receiptLink = rs.getString("reimb_receipt");
+            String status = rs.getString("reimb_status");
+            String type = rs.getString("reimb_type");
+            JSONObject jsonObject = new JSONObject(rs.getString("author"));
+            UserDTO author = new UserDTO(jsonObject.getInt("id"), jsonObject.getString(USERNAME), jsonObject.getString(FIRST_NAME),
+                    jsonObject.getString(LAST_NAME), jsonObject.getString(EMAIL));
+
+            Ticket ticket = new Ticket(id, amount, submitted, status, type);
+            ticket.setAuthor(author);
+
+            if (resolved != null) {
+                JSONObject json = new JSONObject(rs.getString("resolver"));
+                UserDTO resolver = new UserDTO(json.getInt("id"), json.getString(USERNAME), json.getString(FIRST_NAME),
+                        json.getString(LAST_NAME), json.getString(EMAIL));
+                ticket.setResolved(resolved);
+                ticket.setResolver(resolver);
+            }
+            if (description != null) {
+                ticket.setDescription(description);
+            }
+            if (receiptLink != null) {
+                ticket.setReceiptLink(receiptLink);
+            }
+            tickets.add(ticket);
+
+        }
+        return tickets;
     }
 
     public List<Ticket> getTicketFromEmployeeId(int id) throws SQLException {
         try (Connection con = ConnectionUtility.getConnection()) {
 
-            List<Ticket> tickets = new ArrayList<>();
-            String query = "select tk.reimb_id, tk.reimb_amount, tk.reimb_submitted, " +
-                    "tk.reimb_resolved, tk.reimb_description, tk.reimb_receipt, " +
-                    "status.reimb_status, rt.reimb_type, " +
-                    "json_build_object('id', users.ers_users_id, 'username', users.ers_username, 'firstName'" +
-                    ", users.user_first_name, 'lastName', users.user_last_name, 'email', users.user_email) as author, " +
-                    "json_build_object('id', users.ers_users_id, 'username', mng.ers_username, 'firstName', mng.user_first_name, 'lastName', " +
-                    "mng.user_last_name, 'email', mng.user_email) as resolver " +
-                    "from ers_reimbursements as tk " +
-                    "inner join ers_reimbursement_status status " +
-                    "on tk.reimb_status_id = status.reimb_status_id " +
-                    "inner join ers_reimbursement_type rt " +
-                    "on tk.reimb_type_id = rt.reimb_type_id " +
-                    "inner join ers_users users " +
-                    "on tk.reimb_author = users.ers_users_id " +
-                    "left join ers_users mng " +
-                    "on tk.reimb_resolver = mng.ers_users_id "+
-                    "where tk.reimb_author = ?";
+            String query = sql + "where tk.reimb_author = ?";
 
             try (PreparedStatement pstmt = con.prepareStatement(query)) {
                 pstmt.setInt(1, id);
-
-                ResultSet rs = pstmt.executeQuery();
-
-                while (rs.next()) {
-                    int reimb_id = rs.getInt("reimb_id");
-                    double amount = rs.getDouble("reimb_amount");
-                    Timestamp submitted = rs.getObject("reimb_submitted", Timestamp.class);
-                    Timestamp resolved = rs.getObject("reimb_resolved", Timestamp.class);
-                    String description = rs.getString("reimb_description");
-                    String receiptLink = rs.getString("reimb_receipt");
-                    String status = rs.getString("reimb_status");
-                    String type = rs.getString("reimb_type");
-                    JSONObject jsonObject = new JSONObject(rs.getString("author"));
-                    UserDTO author = new UserDTO(jsonObject.getInt("id"), jsonObject.getString(USERNAME), jsonObject.getString(FIRST_NAME),
-                            jsonObject.getString(LAST_NAME), jsonObject.getString(EMAIL));
-
-                    Ticket ticket = new Ticket(reimb_id, amount, submitted, status, type);
-                    ticket.setAuthor(author);
-
-                    if (resolved != null) {
-                        JSONObject json = new JSONObject(rs.getString("resolver"));
-                        UserDTO resolver = new UserDTO(json.getInt("id"), json.getString(USERNAME), json.getString(FIRST_NAME),
-                                json.getString(LAST_NAME), json.getString(EMAIL));
-
-                        ticket.setResolved(resolved);
-                        ticket.setResolver(resolver);
-                    }
-                    if (description != null) {
-                        ticket.setDescription(description);
-                    }
-                    if (receiptLink != null) {
-                        ticket.setReceiptLink(receiptLink);
-                    }
-                    tickets.add(ticket);
-                }
+                return getTickets(pstmt);
             }
-            return tickets;
         }
     }
 
